42af87f6d822b9c8f72ba0c3a4e75442ce7aa3bd
laurenball <lauren.ball@annkissam.com>
2020-09-08 23:26:32 -0400

    Add a button & do somethin when you click it

---
+++ assets/js/geocities.js
@@ -0,0 +1,6 @@
+document.getElementById('geocities-toggle').addEventListener('click', partyTime);
+
+function partyTime() {
+  mainViewport = document.getElementById('page-top');
+  mainViewport.classList.toggle('retro-mode');
+};
