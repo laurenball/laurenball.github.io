f11ca7cfea3a146f471cab70f314196f5e2bd447
laurenball <lauren.ball@annkissam.com>
2020-09-09 01:31:01 -0400

    Add images

--- assets/js/geocities.js
+++ assets/js/geocities.js
@@ -3,4 +3,9 @@ document.getElementById('geocities-toggle').addEventListener('click', partyTime)
 function partyTime() {
   mainViewport = document.getElementById('page-top');
   mainViewport.classList.toggle('geocities-mode');
+
+  imageFuckery();
 };
+
+function imageFuckery() {
+}
