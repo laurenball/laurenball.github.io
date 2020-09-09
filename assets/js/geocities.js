e105a870debecb45729b3aaf8c2754c8b262daab
laurenball <lauren.ball@annkissam.com>
2020-09-09 01:32:59 -0400

    Remove unused js

--- assets/js/geocities.js
+++ assets/js/geocities.js
@@ -3,9 +3,4 @@ document.getElementById('geocities-toggle').addEventListener('click', partyTime)
 function partyTime() {
   mainViewport = document.getElementById('page-top');
   mainViewport.classList.toggle('geocities-mode');
-
-  imageFuckery();
 };
-
-function imageFuckery() {
-}
